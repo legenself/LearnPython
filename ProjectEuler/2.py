8479af880783947e53b7509384d9a0ddd86ea63a
legen <504883942@qq.com>
2016-05-14 19:07:16 +0800

    oh i find my mistake,I had forgot 'the even-valued items'

--- ProjectEuler/2.py
+++ ProjectEuler/2.py
@@ -1,16 +1,15 @@
+
+sum=2
 a=1
 b=2
-ve=[a,b];
-i=2
-while a:
-	temp=ve[i-1]+ve[i-2]
-	if(temp>4000000):
-		break
-	ve.append(temp)
-	i=i+1;
+temp=0
+while temp<4000000:
+	temp=a+b
+	if temp%2==0:
+		sum+=temp
+	a=b
+	b=temp
 
-sum=0
-for x in ve[:]:
-	sum=sum+x
-print(ve)
+
 print(sum)
+
