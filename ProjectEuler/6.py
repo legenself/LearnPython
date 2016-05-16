43e0f54beb8c486819f9e002bb56f442b3ecb85e
legen <504883942@qq.com>
2016-05-16 15:42:10 +0800

    congradulation! I'm the 310573rd person solved this problem

---
+++ ProjectEuler/6.py
@@ -0,0 +1,13 @@
+l=10
+result1=0
+for x in range(101):
+	result1=result1+x**2
+print(result1)
+result2=0
+for x in range(101):
+	result2=result2+x
+
+print(result2)
+
+re=result2**2-result1
+print(re)
